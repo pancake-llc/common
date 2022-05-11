c80dac2efab51553141496bfb61a6c4ceb141da0
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-05-11 21:18:32 +0700

    *Add linq

---
+++ Assets/_Root/Runtime/Linq/Utils/SliceHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Pancake.Linq
+{
+    public static class SliceHelper
+    {
+        public static Span<T> Slice<T>(this T[] array, int start, int len) { return array.AsSpan().Slice(start, len); }
+    }
+}
