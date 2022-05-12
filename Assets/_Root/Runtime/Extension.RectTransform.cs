5f1243d9d1ce12899836cfce707222a4be13f1fd
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-05-12 23:26:21 +0700

    *Fix missing ToScreenRect

--- Assets/_Root/Runtime/Extension.RectTransform.cs
+++ Assets/_Root/Runtime/Extension.RectTransform.cs
@@ -340,5 +340,43 @@ namespace Pancake.Common
             return rect.Contains(new Vector2(other.xMin, other.yMin)) && rect.Contains(new Vector2(other.xMin, other.yMax)) &&
                    rect.Contains(new Vector2(other.xMax, other.yMin)) && rect.Contains(new Vector2(other.xMax, other.yMax));
         }
+        
+        public static Rect ToScreenRect(this RectTransform self, bool startAtBottom = false, Canvas canvas = null, bool localTransform = false)
+        {
+            var corners = new Vector3[4];
+            var screenCorners = new Vector3[2];
+
+            if (localTransform)
+            {
+                self.GetLocalCorners(corners);
+            }
+            else
+            {
+                self.GetWorldCorners(corners);
+            }
+
+            int idx1 = startAtBottom ? 0 : 1;
+            int idx2 = startAtBottom ? 2 : 3;
+
+
+            if (canvas != null && (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace))
+            {
+                screenCorners[0] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[idx1]);
+                screenCorners[1] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[idx2]);
+            }
+            else
+            {
+                screenCorners[0] = RectTransformUtility.WorldToScreenPoint(null, corners[idx1]);
+                screenCorners[1] = RectTransformUtility.WorldToScreenPoint(null, corners[idx2]);
+            }
+
+            if (!startAtBottom)
+            {
+                screenCorners[0].y = Screen.height - screenCorners[0].y;
+                screenCorners[1].y = Screen.height - screenCorners[1].y;
+            }
+
+            return new Rect(screenCorners[0], screenCorners[1] - screenCorners[0]);
+        }
     }
 }
--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Add
 ```
 
 
-- for version 1.1.8
+- for version 1.1.7
 ```csharp
-"com.pancake.common": "https://github.com/pancake-llc/common.git?path=Assets/_Root#1.1.8",
+"com.pancake.common": "https://github.com/pancake-llc/common.git?path=Assets/_Root#1.1.7",
 ```
 
 
