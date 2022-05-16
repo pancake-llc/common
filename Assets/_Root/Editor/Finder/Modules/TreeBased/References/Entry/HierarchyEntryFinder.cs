1faefc7db3db82c99d23559659346a3b6dda92a7
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-05-16 11:48:01 +0700

    *Fix for unity 2019

--- Assets/_Root/Editor/Finder/Modules/TreeBased/References/Entry/HierarchyEntryFinder.cs
+++ Assets/_Root/Editor/Finder/Modules/TreeBased/References/Entry/HierarchyEntryFinder.cs
@@ -1,105 +1,115 @@
-﻿
-
-namespace Pancake.Editor.Finder.Entry
+﻿namespace Pancake.Editor.Finder.Entry
 {
-	using System.Collections.Generic;
-	using UnityEditor;
-	using UnityEngine;
-	using UnityEngine.SceneManagement;
-	using Object = UnityEngine.Object;
-	
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using Object = UnityEngine.Object;
 
-	internal static class HierarchyEntryFinder
-	{
+
+    internal static class HierarchyEntryFinder
+    {
 #if !UNITY_2020_1_OR_NEWER
 		private static int updateStep = -1;
 #endif
-		
-		public static bool FillHierarchyReferenceEntries(Object[] objects, ProcessObjectReferenceHandler processReferenceCallback)
-		{
-			EntryFinder.currentProcessReferenceCallback = processReferenceCallback;
+
+        public static bool FillHierarchyReferenceEntries(Object[] objects, ProcessObjectReferenceHandler processReferenceCallback)
+        {
+            EntryFinder.currentProcessReferenceCallback = processReferenceCallback;
 #if !UNITY_2020_1_OR_NEWER
 			updateStep = -1;
 #endif
+#if UNITY_2021_1_OR_NEWER
+            var prefabStage = UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
+#else
+            var prefabStage = UnityEditor.Experimental.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
+#endif
+            if (prefabStage != null)
+            {
+                return ProcessPrefabForSceneScopeReferences(prefabStage);
+            }
 
-			var prefabStage = UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
-			if (prefabStage != null)
-			{
-				return ProcessPrefabForSceneScopeReferences(prefabStage);
-			}
+            var scenes = GetScenesFromObjects(objects);
+            var rootObjects = GetRootGameObjectsFromScenes(scenes);
+            return ProcessSceneForSceneScopeReferences(rootObjects);
+        }
 
-			var scenes = GetScenesFromObjects(objects);
-			var rootObjects = GetRootGameObjectsFromScenes(scenes);
-			return ProcessSceneForSceneScopeReferences(rootObjects);
-		}
+#if UNITY_2021_1_OR_NEWER
+        private static bool ProcessPrefabForSceneScopeReferences(UnityEditor.SceneManagement.PrefabStage prefabStage)
+#else
+        private static bool ProcessPrefabForSceneScopeReferences(UnityEditor.Experimental.SceneManagement.PrefabStage prefabStage)
+#endif
 
-		private static bool ProcessPrefabForSceneScopeReferences(UnityEditor.SceneManagement.PrefabStage prefabStage)
-		{
-			EntryFinder.currentLocation = Location.PrefabAssetGameObject;
-			return !TraverseTools.TraversePrefabGameObjects(prefabStage.prefabContentsRoot, false, true, EntryFinder.OnGameObjectTraverse);
-		}
-		
-		private static bool ProcessSceneForSceneScopeReferences(GameObject[] rootObjects)
-		{
-			EntryFinder.currentLocation = Location.SceneGameObject;
-			return !TraverseTools.TraverseRootGameObjects(rootObjects, false, true, EntryFinder.OnGameObjectTraverse, OnRootTraverse);
-		}
+        {
+            EntryFinder.currentLocation = Location.PrefabAssetGameObject;
+            return !TraverseTools.TraversePrefabGameObjects(prefabStage.prefabContentsRoot, false, true, EntryFinder.OnGameObjectTraverse);
+        }
 
-		private static void OnRootTraverse(int index, int total, out bool canceled)
-		{
+        private static bool ProcessSceneForSceneScopeReferences(GameObject[] rootObjects)
+        {
+            EntryFinder.currentLocation = Location.SceneGameObject;
+            return !TraverseTools.TraverseRootGameObjects(rootObjects,
+                false,
+                true,
+                EntryFinder.OnGameObjectTraverse,
+                OnRootTraverse);
+        }
+
+        private static void OnRootTraverse(int index, int total, out bool canceled)
+        {
 #if !UNITY_2020_1_OR_NEWER
 			if (updateStep == -1)
 			{
 				updateStep = Math.Max(total / ProjectSettings.UpdateProgressStep, 1);
 			}
 #endif
-			
-			canceled =
+
+            canceled =
 #if !UNITY_2020_1_OR_NEWER
 				(index < 10 || index % updateStep == 0) &&
 #endif
-				EditorUtility.DisplayCancelableProgressBar(string.Format(ReferencesFinder.ProgressCaption, 2, ReferencesFinder.PhasesCount), 
-				           string.Format(ReferencesFinder.ProgressText, "Filling reference details", index + 1, total),
-				           (float)index / total);
-		}
-		
-		private static Scene[] GetScenesFromObjects(Object[] objects)
-		{
-			var result = new List<Scene>(objects.Length);
-			
-			foreach (var o in objects)
-			{
-				if (o is Component)
-				{
-					var scene = (o as Component).gameObject.scene;
-					if (!result.Contains(scene))
-					{
-						result.Add(scene);
-					}
-				}
-				else if (o is GameObject)
-				{
-					var scene = (o as GameObject).scene;
-					if (!result.Contains(scene))
-					{
-						result.Add(scene);
-					}
-				}
-			}
+                EditorUtility.DisplayCancelableProgressBar(string.Format(ReferencesFinder.ProgressCaption, 2, ReferencesFinder.PhasesCount),
+                    string.Format(ReferencesFinder.ProgressText, "Filling reference details", index + 1, total),
+                    (float) index / total);
+        }
 
-			return result.ToArray();
-		}
+        private static Scene[] GetScenesFromObjects(Object[] objects)
+        {
+            var result = new List<Scene>(objects.Length);
 
-		private static GameObject[] GetRootGameObjectsFromScenes(Scene[] scenes)
-		{
-			var result = new List<GameObject>();
-			
-			foreach (var scene in scenes)
-			{
-				result.AddRange(scene.GetRootGameObjects());
-			}
+            foreach (var o in objects)
+            {
+                if (o is Component)
+                {
+                    var scene = (o as Component).gameObject.scene;
+                    if (!result.Contains(scene))
+                    {
+                        result.Add(scene);
+                    }
+                }
+                else if (o is GameObject)
+                {
+                    var scene = (o as GameObject).scene;
+                    if (!result.Contains(scene))
+                    {
+                        result.Add(scene);
+                    }
+                }
+            }
 
-			return result.ToArray();
-		}
-	}
+            return result.ToArray();
+        }
+
+        private static GameObject[] GetRootGameObjectsFromScenes(Scene[] scenes)
+        {
+            var result = new List<GameObject>();
+
+            foreach (var scene in scenes)
+            {
+                result.AddRange(scene.GetRootGameObjects());
+            }
+
+            return result.ToArray();
+        }
+    }
 }
