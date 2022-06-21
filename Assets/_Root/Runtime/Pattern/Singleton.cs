dfbe2878ddeba34d26fbefc51b018d05a8b0b75a
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-06-21 21:45:18 +0700

    *add singleton

---
+++ Assets/_Root/Runtime/Pattern/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pancake.Common
+{
+    public class Singleton<T> where T : class
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null) instance = Activator.CreateInstance<T>();
+                return instance;
+            }
+        }
+    }
+}
